47d0bfaf276e06138427b56d439fa04be62c8ff9
Kachimemba <kachimemba@gmail.com>
2023-06-16 14:46:19 -0700

    Print numbers5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,16 +2,17 @@
 
 /**
  * main - prints numbers 0 to 9
- * Rerurn: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
-	int i;
 
-	for (i = 0; i < 10; i++)
-	{
-		printf("%d", i);
-	}
-	printf('\n');
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
 	return (0);
 }
